5ad09bc978fc3d93d6251adc954d786bf0639c7b
kkazlov <kkazlov.it@gmail.com>
2021-09-23 19:53:09 +0300

    lesson1 - bags fixed

--- webix.js
+++ webix.js
@@ -1,216 +1,154 @@
-const small_film_set = [
-    {
-        id: 1,
-        title: "The Shawshank Redemption",
-        year: 1994,
-        votes: 678790,
-        rating: 9.2,
-        rank: 1,
-        category: "Thriller",
-    },
-    {
-        id: 2,
-        title: "The Godfather",
-        year: 1972,
-        votes: 511495,
-        rating: 9.2,
-        rank: 2,
-        category: "Crime",
-    },
-    {
-        id: 3,
-        title: "The Godfather: Part II",
-        year: 1974,
-        votes: 319352,
-        rating: 9.0,
-        rank: 3,
-        category: "Crime",
-    },
-    {
-        id: 4,
-        title: "The Good, the Bad and the Ugly",
-        year: 1966,
-        votes: 213030,
-        rating: 8.9,
-        rank: 4,
-        category: "Western",
-    },
-    {
-        id: 5,
-        title: "Pulp fiction",
-        year: 1994,
-        votes: 533848,
-        rating: 8.9,
-        rank: 5,
-        category: "Crime",
-    },
-    {
-        id: 6,
-        title: "12 Angry Men",
-        year: 1957,
-        votes: 164558,
-        rating: 8.9,
-        rank: 6,
-        category: "Western",
-    },
-];
+import small_film_set from "./db.js";
+
+const label = {
+    view: "label",
+    label: "My App",
+    autowidth: true,
+    css: "header__label",
+};
+
+const btnHeader = {
+    view: "button",
+    type: "icon",
+    icon: "wxi-user",
+    label: "Profile",
+    align: "right",
+    autowidth: true,
+
+    css: "header__btn",
+};
+
+const contentList = {
+    view: "list",
+    template: "#title#",
+    borderless: true,
+    scroll: false,
+    select: true,
+    data: [
+        { id: 1, title: "Dashboard" },
+        { id: 2, title: "Users" },
+        { id: 3, title: "Products" },
+        { id: 4, title: "Locations" },
+    ],
+};
+
+const contentStatus = {
+    view: "template",
+    borderless: true,
+    autoheight: true,
+    template: `
+        <span class='webix_icon wxi-check'></span> 
+        Connected
+    `,
+    css: 'content__status'
+}
+
+const formInputs = {
+    rows: [
+        {
+            template: "Edit films",
+            type: "section",
+        },
+
+        {
+            view: "text",
+            label: "Title",
+            name: "title",
+        },
+        {
+            view: "text",
+            label: "Year",
+            name: "year",
+        },
+
+        {
+            view: "text",
+            label: "Rating",
+            name: "rating",
+        },
+
+        {
+            view: "text",
+            label: "Votes",
+            name: "votes",
+        },
+    ],
+}
+
+const formBtns = {
+    cols: [
+        {
+            view: "button",
+            value: "Add new",
+            css: "webix_primary",
+        },
+        {
+            view: "button",
+            value: "Clear",
+        },
+    ],
+}
 
 webix.ready(() => {
     webix.ui({
-        
         rows: [
-            //header
-            {   
-                
+            {   //header
+                view: "toolbar",
+                id: "myToolbar",
                 paddingX: 10,
-
                 cols: [
-                    {
-                        view: "label",
-                        label: "My App",
-                        gravity: 1,
-                        autowidth: true,
-                        css: "header__label",
-                    },
-
-                    { gravity: 2 },
-
-                    {
-                        view: "button",
-                        type: "icon",
-                        icon: "wxi-user",
-                        label: "Profile",
-                        align: "right",
-                        maxWidth: 150,
-                        gravity: 1,
-
-                        css: "header__btn",
-                    },
+                    label,
+                    {},
+                    btnHeader
                 ],
-                css: "header",
+                css:"webix_dark",
             },
 
-            //content
-            {   
-                
+            {   //content
                 cols: [
-                    // list and status
-                    {
+                    {   //content-list
                         type: "clear",
-                        gravity: 1,
-                        
                         rows: [
-                            //content-list
-                            {
-                                view: "list",
-                                template: "#title#",
-                                scroll: false,
-                                select: true,
-                                data: [
-                                    { id: 1, title: "Dashboard" },
-                                    { id: 2, title: "Users" },
-                                    { id: 3, title: "Products" },
-                                    { id: 4, title: "Locations" },
-                                ],
-                            },
-
-                            //content-status
-                            {
-                                view: "button",
-                                type: "icon",
-                                icon: "wxi-check",
-                                label: "Connected",
-                                align: "center",
-                                
-                            },
+                            contentList,
+                            contentStatus
                         ],
-                        
                         css: "content__list",
                     },
 
-                    { 
-                        view: "resizer",
-                    },
+                    { view: "resizer" },
 
-                    // dataTable
-                    {
+                    {   //content-dataTable
                         gravity: 4,
                         view: "datatable",
                         autoConfig: true,
+                        scrollX: false,
 
                         data: small_film_set,
-                        
                     },
-
-                    //form
-                    {
-                        autoheight: false,
+                    
+                    {   //form
                         gravity: 2,
                         view: "form",
-                        
+
                         elements: [
-                            {
-                                rows: [
-                                    {
-                                        template: "Edit films",
-                                        type: "section",
-                                    },
-
-                                    {
-                                        view: "text",
-                                        label: "Title",
-                                        name: "title",
-                                    },
-                                    {
-                                        view: "text",
-                                        label: "Year",
-                                        name: "year",
-                                    },
-
-                                    {
-                                        view: "text",
-                                        label: "Rating",
-                                        name: "rating",
-                                    },
-
-                                    {
-                                        view: "text",
-                                        label: "Votes",
-                                        name: "votes",
-                                    },
-                                ],
-                            },
-
-                            {
-                                cols: [
-                                    {
-                                        view: "button",
-                                        value: "Add new",
-                                        css: "webix_primary",
-                                    },
-                                    {
-                                        view: "button",
-                                        value: "Clear",
-                                    },
-                                ],
-                            },
+                            formInputs,
+                            formBtns,
+                            {}
                         ],
                     },
                 ],
             },
 
-            //footer
-            {
+            
+            {   //footer
                 autoheight: true,
-                
                 view: "template",
                 template: `
-                    <footer>
-                        The software is provided by
-                        <a href="https://webix.com/">https://webix.com/</a>
-                        All rights reserved ©
-                    </footer>
+                    The software is provided by
+                    <a href="https://webix.com/">https://webix.com/</a>
+                    All rights reserved ©
                 `,
+                css: "footer"
             },
         ],
     });
